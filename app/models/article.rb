c1e1b683eb785c9f953df95655ed96ee677bbb65
novneetnov <novneet346iith@gmail.com>
2016-01-19 18:27:42 +0530

    Update All Views with Categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
 	belongs_to :user
-	has_many :article_categories
+	has_many :article_categories, dependent: :destroy
 	has_many :categories, through: :article_categories
 	validates	:title, presence: true, length: {minimum: 3, maximum: 50}
 	validates :description, presence: true, length: {minimum: 10, maximum: 300}
